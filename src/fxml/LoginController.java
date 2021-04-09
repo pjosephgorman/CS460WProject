f003ce59b44a0851fd2aac264a486f5b7c4b9198
Matt <maiello@hartford.edu>
2021-04-09 18:50:10 -0400

    Errors in login

--- src/fxml/LoginController.java
+++ src/fxml/LoginController.java
@@ -3,6 +3,7 @@ package fxml;
 import JavaSrc.Client;
 import JavaSrc.Util;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -10,20 +11,23 @@ public class LoginController
 {
 	public TextField uname;
 	public PasswordField pwd;
-	//TODO add 'Text'/'Label' for errors
+	public Label error;
 	
 	public void doLogin(ActionEvent event)
 	{
 		if(uname.getText().equals("") || pwd.getText().equals(""))
 		{
-			//TODO display error message 'missing required fields'
+			error.setText("Missing required fields!");
+			return;
 		}
-		else Client.singleton.runCommand("login " + (uname.getText().replaceAll(" ", "")) + " " + Util.hash(pwd.getText().replaceAll(" ", "")));
+		error.setText("");
+		Client.singleton.runCommand("login " + (uname.getText().replaceAll(" ", "")) + " " + Util.hash(pwd.getText().replaceAll(" ", "")));
 	}
 	
 	public void clear()
 	{
 		uname.setText("");
 		pwd.setText("");
+		error.setText("");
 	}
 }
