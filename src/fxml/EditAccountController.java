007a73959c89617991344dbf84ba86946f3afb7a
Matt <maiello@hartford.edu>
2021-04-10 15:07:44 -0400

    Edit Account GUI

--- src/fxml/EditAccountController.java
+++ src/fxml/EditAccountController.java
@@ -1,6 +1,32 @@
 package fxml;
 
-public class EditAccountController
-{
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
+public class EditAccountController implements SceneController
+{
+	public TextField username;
+	public TextField newPassword;
+	public TextField confirmPassword;
+	public TextField firstName;
+	public TextField middleName;
+	public TextField lastName;
+	public ChoiceBox role;
+	public TextField phoneNumber;
+	public TextField emailAddress;
+	public Button cancelButton;
+	public Button submitButton;
+	
+	@Override
+	public void clear()
+	{
+	
+	}
+	
+	@Override
+	public void error(String msg)
+	{
+	
+	}
 }
