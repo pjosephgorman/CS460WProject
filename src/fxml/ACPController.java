2b33b80f81bf8207c0a8ac46f3442e4eaea10a9a
Matt <maiello@hartford.edu>
2021-04-11 14:10:05 -0400

    proper table in acp

--- src/fxml/ACPController.java
+++ src/fxml/ACPController.java
@@ -4,9 +4,13 @@ import JavaSrc.Client;
 import JavaSrc.Data.UserInfo;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class ACPController implements SceneController
 {
+	public VBox vBox;
 	public Button newUserButton;
 	public Button editButton;
 	public Button deleteButton;
@@ -28,6 +32,24 @@ public class ACPController implements SceneController
 		Client.singleton.setMainMenu();
 	}
 	
+	public void load(UserInfo info){
+		HBox hBox = new HBox();
+		Label nameLabel = new Label(info.uname + " (" + info.fname + (info.mname == null ? "" : " " + info.mname) + " " + info.lname + ")");
+		hBox.getChildren().add(nameLabel);
+		Button edit = new Button("Edit");
+		edit.setOnAction(e ->{
+			Client.singleton.runCommand("edituser " + info.id);
+		});
+		hBox.getChildren().add(edit);
+		Button delete = new Button("Delete");
+		delete.setOnAction(e ->{
+			Client.singleton.runCommand("deleteuser " + info.id);
+		});
+		hBox.getChildren().add(delete);
+		vBox.getChildren().add(hBox);
+		System.out.println(info);
+	}
+	
 	@Override
 	public void clear()
 	{
@@ -43,6 +65,8 @@ public class ACPController implements SceneController
 	@Override
 	public void updateInfo(UserInfo info)
 	{
-	
+		for(int i = 0; i< 100; i++){
+			load(info);
+		}
 	}
 }
