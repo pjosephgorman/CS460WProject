cccbd0239837ff3f191542204cb4b8ef3ceda47f
Matt <maiello@hartford.edu>
2021-04-11 13:17:46 -0400

    fixing aesthetics and hooking up button functionality

--- src/fxml/EditAccountController.java
+++ src/fxml/EditAccountController.java
@@ -1,15 +1,18 @@
 package fxml;
 
+import JavaSrc.Client;
 import JavaSrc.Data.UserInfo;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class EditAccountController implements SceneController
 {
 	public TextField username;
-	public TextField oldpassword;
+	public TextField oldPassword;
 	public TextField newPassword;
 	public TextField confirmPassword;
 	public TextField firstName;
@@ -21,19 +24,33 @@ public class EditAccountController implements SceneController
 	public Button cancelButton;
 	public Button submitButton;
 	
+	@FXML
+	private Label error;
+	
 	public void doCancel(ActionEvent event){
-		//TODO create cancel feature to go back
+		clear();
+		Client.singleton.setMainMenu();
 	}
 	
 	public void doSubmit(ActionEvent event){
 		//TODO create submit button that sends data to database
+		if(oldPassword.getText().isBlank() || newPassword.getText().isBlank() || confirmPassword.getText().isBlank()
+				|| phoneNumber.getText().isBlank() || emailAddress.getText().isBlank())
+		{
+			error.setText("Missing required fields!");
+			return;
+		}
 	}
 	
 	
 	@Override
 	public void clear()
 	{
-	
+		oldPassword.setText("");
+		newPassword.setText("");
+		confirmPassword.setText("");
+		phoneNumber.setText("");
+		emailAddress.setText("");
 	}
 	
 	@Override
