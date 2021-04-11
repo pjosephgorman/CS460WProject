242cdd379ff97348c583361c83395c6c93c60b82
Matt <maiello@hartford.edu>
2021-04-11 17:59:43 -0400

    patient test user interface

---
+++ src/fxml/PatientTestsController.java
@@ -0,0 +1,32 @@
+package fxml;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+
+public class PatientTestsController
+{
+	public RadioButton xRay;
+	public RadioButton ctScan;
+	public RadioButton MRI;
+	public RadioButton redBloodTest;
+	public RadioButton whiteBloodTest;
+	public RadioButton liverTest;
+	public RadioButton kidneyTest;
+	public RadioButton electrolyteTest;
+	public CheckBox stoolTest;
+	public CheckBox urineTest;
+	public Button cancel;
+	public Button submit;
+	
+	public void doSubmit(ActionEvent event)
+	{
+	
+	}
+	
+	public void doCancel(ActionEvent event)
+	{
+	
+	}
+}
